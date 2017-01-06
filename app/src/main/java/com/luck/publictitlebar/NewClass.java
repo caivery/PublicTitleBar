ad42b0e8d5f36ee5c99a38fe1db7095ba3145780
luck <893855882@qq.com>
2017-01-06 10:15:10 +0800

    new class

---
+++ app/src/main/java/com/luck/publictitlebar/NewClass.java
@@ -0,0 +1,11 @@
+package com.luck.publictitlebar;
+
+/**
+ * author：luck
+ * project：PublicTitleBar
+ * package：com.luck.publictitlebar
+ * email：893855882@qq.com
+ * data：17/1/6
+ */
+public class NewClass {
+}
